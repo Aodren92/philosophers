1e680f1d7374e198a2fd229fd78d62847c0a0494
Aodren Bary <abary@student.42.fr>
2017-12-02 20:53:23 +0100

    add right pos baguette

--- srcs/philo_display_baguettes.c
+++ srcs/philo_display_baguettes.c
@@ -1,5 +1,6 @@
 #include "philosophers.h"
 
+# define DEBUG_POS_NORM 0
 # define DEBUG_POS_RIGHT 7
 # define DEBUG_POS_LEFT 14
 void	philo_display_baguettes(t_env *e)
@@ -10,8 +11,8 @@ void	philo_display_baguettes(t_env *e)
 	while (i < 15)
 	{
 		SDL_RenderCopyEx(e->sys.renderer, e->texture[i].tex,
-				&e->texture[i].rect_s, &e->pos[i + DEBUG_POS_RIGHT].rect_d,
-				e->pos[i + DEBUG_POS_RIGHT].angle, e->pos[i + DEBUG_POS_RIGHT].center,
+				&e->texture[i].rect_s, &e->pos[i + DEBUG_POS_LEFT].rect_d,
+				e->pos[i + DEBUG_POS_LEFT].angle, e->pos[i + DEBUG_POS_LEFT].center,
 				0);
 		++i;
 	}
--- srcs/philo_init_pos.c
+++ srcs/philo_init_pos.c
@@ -120,7 +120,6 @@ void	philo_init_pos(t_pos *pos)
 	pos[14].angle = 285;
 	pos[14].center = 0;
 	//baguttes pos right
-	//
 	pos[15].rect_d.x = 250;
 	pos[15].rect_d.y = 340;
 	pos[15].rect_d.w = 30;
@@ -170,4 +169,57 @@ void	philo_init_pos(t_pos *pos)
 	pos[21].rect_d.h = 85;
 	pos[21].angle = 0;
 	pos[21].center = 0;
+
+
+	//baguette pos left
+	pos[22].rect_d.x = 120;
+	pos[22].rect_d.y = 440;
+	pos[22].rect_d.w = 30;
+	pos[22].rect_d.h = 85;
+	pos[22].angle = 0;
+	pos[22].center = 0;
+
+
+	pos[23].rect_d.x = 160;
+	pos[23].rect_d.y = 340;
+	pos[23].rect_d.w = 30;
+	pos[23].rect_d.h = 75;
+	pos[23].angle = 0;
+	pos[23].center = 0;
+
+	pos[24].rect_d.x = 360;
+	pos[24].rect_d.y = 290;
+	pos[24].rect_d.w = 30;
+	pos[24].rect_d.h = 75;
+	pos[24].angle = 0;
+	pos[24].center = 0;
+
+	pos[25].rect_d.x = 570;
+	pos[25].rect_d.y = 220;
+	pos[25].rect_d.w = 30;
+	pos[25].rect_d.h = 75;
+	pos[25].angle = 0;
+	pos[25].center = 0;
+
+	pos[26].rect_d.x = 820;
+	pos[26].rect_d.y = 285;
+	pos[26].rect_d.w = 30;
+	pos[26].rect_d.h = 75;
+	pos[26].angle = 0;
+	pos[26].center = 0;
+
+	pos[27].rect_d.x = 1040;
+	pos[27].rect_d.y = 330;
+	pos[27].rect_d.w = 30;
+	pos[27].rect_d.h = 75;
+	pos[27].angle = 0;
+	pos[27].center = 0;
+
+	pos[28].rect_d.x = 1100;
+	pos[28].rect_d.y = 450;
+	pos[28].rect_d.w = 30;
+	pos[28].rect_d.h = 85;
+	pos[28].angle = 0;
+	pos[28].center = 0;
+
 }
